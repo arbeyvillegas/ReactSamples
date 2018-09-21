bde27395937fd699cacb84f946c4521b22ab1bac
Arbey <arbpoe@hotmail.com>
2018-09-20 21:59:13 -0500

    Change activity comment

--- 05-Session/activity/src/index.js
+++ 05-Session/activity/src/index.js
@@ -1,5 +1,5 @@
-//Create a temperature calculator that calculates 
-//whether the water would boil at a given temperature
+//Create a speed calculator that validate 
+//whether the speed limit is surpassed
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SpeedCalculator from './speed-calculator'
